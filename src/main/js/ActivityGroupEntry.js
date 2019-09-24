fc14027915543635ea8434383da577faa20ec4c6
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-09-24 09:24:23 +0200

    small improvments

--- src/main/js/ActivityGroupEntry.js
+++ src/main/js/ActivityGroupEntry.js
@@ -1,7 +1,6 @@
 //@flow
 import React from "react";
 import injectSheet from "react-jss";
-import classNames from "classnames";
 import type { ActivityGroup } from "./ActivityGroup";
 import { ChangesetList, Icon } from "@scm-manager/ui-components";
 import { translate } from "react-i18next";
@@ -14,13 +13,13 @@ const styles = {
     fontSize: "1.25rem",
 
     "& small": {
-      fontSize: "0.75rem"
+      fontSize: "0.875rem"
     }
   },
   wrapper: {
     margin: "1rem 0 2rem",
     padding: "1rem",
-    border: "1px solid var(--border)",
+    border: "1px solid #dbdbdb", // $border
     borderRadius: "4px"
   }
 };
@@ -67,22 +66,22 @@ class ActivityGroupEntry extends React.Component<Props, State> {
         </div>
       );
     }
+
     return (
       <div className={classes.activityGroup}>
-        <h3
-          className={classNames("has-cursor-pointer", classes.headline)}
-          onClick={this.toggleCollapse}
-        >
-          <Icon name={icon} color="default" /> {group.repository.namespace}/
-          {group.repository.name} - {group.repository.type}{" "}
-          <small className="has-text-grey-light">
-            (
-            {t("scm-activity-plugin.changeset", {
-              count: group.changesets.length
-            })}
-            )
-          </small>
-        </h3>
+        <div className="has-cursor-pointer" onClick={this.toggleCollapse}>
+          <h3 className={classes.headline}>
+            <Icon name={icon} color="default" /> {group.repository.namespace}/
+            {group.repository.name} - {group.repository.type}{" "}
+            <small className="has-text-grey-light">
+              (
+              {t("scm-activity-plugin.changeset", {
+                count: group.changesets.length
+              })}
+              )
+            </small>
+          </h3>
+        </div>
         {content}
       </div>
     );
